ec654fbe11f7680433f3df6ff968386d559b90ae
BLipsky <BenLipsky@icloud.com>
2025-02-18 11:19:03 -0500

    10

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ document.addEventListener("click", function() {
     document.querySelector(".lefty").classList.add("move");
     document.querySelector(".righty").classList.add("move");
   });
-  
+
+  document.addEventListener("click", function() {
+    setTimeout(function() {
+        document.querySelector(".temp").classList.remove("temp");
+    }, 7000);
+});
