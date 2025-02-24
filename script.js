3b58a1147e6dfdf4f0d6ed295ed9005726fae5d7
s-jaswal <sjasdocs@gmail.com>
2025-02-24 00:34:42 -0500

    Photos added, editing gallery, trying to make gallery work, changing background

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-document.addEventListener("click", function() {
+document.addEventListener("click", function () {
     document.querySelector(".lefty").classList.add("move");
     document.querySelector(".righty").classList.add("move");
-  });
+});
 
-  document.addEventListener("click", function() {
-    setTimeout(function() {
+document.addEventListener("click", function () {
+    setTimeout(function () {
         document.querySelector(".temp").classList.remove("temp");
     }, 7000);
 });
-    var myCarousel = document.querySelector('#carouselExampleIndicators');
-    var carousel = new bootstrap.Carousel(myCarousel, {
-        interval: 2000,
-        wrap: true
-    });
+var myCarousel = document.querySelector('#carouselExampleIndicators');
+var carousel = new bootstrap.Carousel(myCarousel, {
+    interval: 2000,
+    wrap: true
+});
+
+
+// gallery js start 
+$('.gallery ul li a').click(function () {
+    var itemID = $(this).attr('href');
+    $('.gallery ul').addClass('item_open');
+    $(itemID).addClass('item_open');
+    return false;
+});
+$('.close').click(function () {
+    $('.port, .gallery ul').removeClass('item_open');
+    return false;
+});
+
+$(".gallery ul li a").click(function () {
+    $('html, body').animate({
+        scrollTop: parseInt($("#top").offset().top)
+    }, 400);
+});
+// gallery js end 
